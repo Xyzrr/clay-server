be8c8765cd5d5b4459dc09c7e2b498c32c981056
John Qian <johnlongqian@gmail.com>
2021-01-16 13:53:16 -0700

    bring back homepage

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ import path from "path";
 const PORT = process.env.PORT || 5000;
 
 const server = express()
-  .use(express.static(path.join(__dirname, "public")))
+  .use(express.static(path.join(__dirname, "../public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
-  // .get("/", (req, res) => res.render("pages/index"))
+  .get("/", (req, res) => res.render("pages/index"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const defaultValue = [
